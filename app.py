5fc48c78135093410a923a57c172891ec636788b
mehulmukherjee <50199817+mehulmukherjee@users.noreply.github.com>
2022-04-09 14:24:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,52 @@
-# import
-# essential imports
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Output, Input
-
-import plotly.express as px
-import math
-from dash import no_update
-
-import pandas as pd
+import dash_bootstrap_components as dbc
+import dash_html_components as html
 import numpy as np
-import json
+import pandas as pd
+import matplotlib as mpl
+import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does
+from whitenoise import WhiteNoise   #for serving static files on Heroku
 
-# this css creates columns and row layout
+# my header
+import plotly.express as px
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+## Instantiate dash app
+# app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY]) 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-## Uncomment the following line for runnning in Google Colab
-app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
+# Define the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
+server = app.server 
 
-## Uncomment the following line for running in a webbrowser
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
-  "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-  "Amount": [4, 1, 2, 2, 4, 5],
-  "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-
-# layout
 app.layout = html.Div(children=[
-  html.H1(children='Hello Dash'),#'<h1> this is a header </h1>'
-  #
-  # html.Div(children='''
-  #     Dash: A web application framework for Python.
-  # '''),
+    html.H1(children='Hello Dash'),#'<h1> this is a header </h1>'
+    #
+    # html.Div(children='''
+    #     Dash: A web application framework for Python.
+    # '''),
 
-  dcc.Graph(
-    id='example-graph',
-    figure=fig
-  )
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
-
-  
-# run the code
-# uncomment the following line to run in Google Colab
-app.run_server(mode='inline', port=8030)
-
-# uncomment the following lines to run in Browser via command line/terminal
-#if __name__ == '__main__':
-#  app.run_server(debug=True, host='127.0.0.1', port=8000)
+# Run dash app
+if __name__ == "__main__":
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
+ 
